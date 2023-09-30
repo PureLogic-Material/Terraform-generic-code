55077121f68e5bff6d4c6a5a762d08441d71ddde
zain janjua <zainjanjua74@gmail.com>
2023-09-30 13:29:32 +0500

    First-commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+
+variable "main_region" {
+  type    = string
+  default = "us-east-1"
+}
+# variable "access-key" {
+#   type    = string
+#   default = "AKIA3NM6P52VCYDTF7GT"
+# }
+
+# variable "secret-key" {
+#   type    = string
+#   default = "QZAK7j1FnlGA74SNlMKt8n1/KbpUSSA/NTzIG6b4"
+# }
+provider "aws" {
+  region     = var.main_region
+  access_key = var.access-key
+  secret_key = var.secret-key
+}
+
+module "vpc" {
+  source = "./module/VPC"
+  region = var.main_region
+}
+
+resource "aws_instance" "my-instance" {
+  ami           = module.vpc.ami_id
+  subnet_id     = module.vpc.subnet_id
+  instance_type = "t2.micro"
+}
